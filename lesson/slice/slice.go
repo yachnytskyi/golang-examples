313dd89d6ec589a0449d05cba2f10306775b179c
yachnytskyi <yachnytskyi1992@gmail.com>
2025-02-25 10:40:25 +0200

    Refactoring

--- lesson/slice/slice.go
+++ lesson/slice/slice.go
@@ -2,42 +2,52 @@ package main
 
 import "fmt"
 
+func modifyArray(arr [3]int) {
+    arr[0] = 99
+    fmt.Println("Inside function:", arr) 
+}
+
 func modifySlice(s []int) {
-	fmt.Println("Inside function:", s) // Output: [0, 0, 0]
-	s[0] = 100                         // Modify an element
+	fmt.Println("Inside function:", s) 
+	s[0] = 100                        
 }
 
 func addElement(s []int) {
 	s = append(s, 4)
-	fmt.Println("Inside function:", s) // Output: [1, 2, 3, 4]
+	s = append(s, 5)
+	fmt.Println("Inside function:", s) 
 }
 
 func modifyAndReturnSlice(s []int) []int {
-	fmt.Println("Inside function:", s) // Output: [0, 0, 0]
+	fmt.Println("Inside function:", s) 
 	s[0] = 100
 	s = append(s, 4)
 	return s
 }
 
 func addElementByPointer(s *[]int) {
-	fmt.Println("Inside function:", *s) // Output: [1, 2, 3]
+	fmt.Println("Inside function:", *s)
 	*s = append(*s, 4)
 }
 
 func main() {
+	myArray := [3]int{1, 2, 3}
+    modifyArray(myArray)
+    fmt.Println("Outside function:", myArray) 
+
 	mySlice := []int{1, 2, 3}
 	modifySlice(mySlice)
-	fmt.Println("Outside function:", mySlice) // Output: [100, 2, 3]
+	fmt.Println("Outside function:", mySlice) 
 
 	mySlice = make([]int, 3, 4)
 	addElement(mySlice)
-	fmt.Println("Outside function:", mySlice) // Output: [1, 2, 3]
+	fmt.Println("Outside function:", mySlice)
 
 	mySlice = make([]int, 3, 4)
 	mySlice = modifyAndReturnSlice(mySlice)
-	fmt.Println("Outside function:", mySlice) // Output: [100, 0, 0, 4]
+	fmt.Println("Outside function:", mySlice) 
 
 	newSlice := []int{1, 2, 3}
 	addElementByPointer(&newSlice)
-	fmt.Println("Outside function:", newSlice) // Output: [1, 2, 3, 4]
+	fmt.Println("Outside function:", newSlice) 
 }
